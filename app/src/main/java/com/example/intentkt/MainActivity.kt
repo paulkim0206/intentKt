54e4d3c45069c30a9683f2cb5894aa5405bab739
paulkim0206 <paulkim0206@gmail.com>
2021-06-08 16:12:35 +0900

    Add ImageView practice

--- app/src/main/java/com/example/intentkt/MainActivity.kt
+++ app/src/main/java/com/example/intentkt/MainActivity.kt
@@ -4,20 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.btn_a)
+        val btn = findViewById<Button>(R.id.btn_main)
 
         btn.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)
             val msg = findViewById<TextView>(R.id.tv_sendMsg)
             intent.putExtra("msg", msg.text.toString())
             startActivity(intent)
+            finish() // 자기 자신 액티비티를 파괴한다
+        }
+
+        findViewById<Button>(R.id.btn_toast).setOnClickListener {
+            findViewById<ImageView>(R.id.iv_profile).setImageResource(R.drawable.android2) // 이미지뷰에 새로운 이미지 등록
+            Toast.makeText(this, "Clicked!!", Toast.LENGTH_SHORT).show()
         }
     }
 }
